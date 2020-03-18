b548d1aa8e990c0ee73fb619a8203dfa58e0eddf
Mathieu Gueribout <mgueribout@gmail.com>
2020-03-18 13:01:54 +0100

    init CardOfTheRings project

--- money/app/src/main/java/com/tchoupi/money/CurrencyListActivity.java
+++ money/app/src/main/java/com/tchoupi/money/CurrencyListActivity.java
@@ -7,7 +7,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.util.JsonReader;
 import android.util.Log;
 
 import org.jetbrains.annotations.NotNull;
